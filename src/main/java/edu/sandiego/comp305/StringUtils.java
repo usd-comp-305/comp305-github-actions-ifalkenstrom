c8602dee20af31014c049ea6d4284506679fd1cc
Ian Falkenstrom <ifalkenstrom@sandiego.edu>
2026-04-26 23:49:37 -0700

    fixed bugs

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -4,15 +4,23 @@ package edu.sandiego.comp305;
  * Utility class for string operations.
  */
 public class StringUtils {
+
+    private StringUtils() {
+        // Prevent instantiation
+    }
+
     /**
      * Reverses the provided string.
      * @param s The string to reverse.
      * @return The reversed string.
      */
-    public static String reverseString(String s) {
+    public static String reverseString(final String s) {
         if (s == null) {
             return null;
         }
         return new StringBuilder(s).reverse().toString();
     }
 }
+
+
+
